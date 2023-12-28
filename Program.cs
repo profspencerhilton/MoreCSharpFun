21c43e8ee20e89ad0a3174610d062d561b96a9c7
Record <Record@SPENCER-XPS8930>
2023-12-28 11:29:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using MoreCSharpFun;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName();
+    }
+}
